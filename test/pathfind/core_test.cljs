a1cf0ce3af2fe0fb1ca0def8f823ebaf5038940e
Crispin Wellington <retrogradeorbit@gmail.com>
2016-11-16 21:26:31 +0800

    first test

--- test/pathfind/core_test.cljs
+++ test/pathfind/core_test.cljs
@@ -2,4 +2,23 @@
   (:require [cljs.test :refer-macros [deftest is]]
             [pathfind.core :as core]))
 
-(deftest foo (is (= 4 4)))
+(deftest reduce-state
+  (let [start [0 0]
+        result (core/reduce-state-over-neighbors
+                (core/->state #{} #{start} {} {start 0} {start 30})
+                [0 0] [10 10] (core/neighbors-for [0 0]))
+        {:keys [closed-set open-set came-from g-score f-score]} result
+        ]
+    (println result)
+    (is (= came-from
+           {[-1 -1] [0 0]
+            [-1 0]  [0 0]
+            [-1 1]  [0 0]
+            [0 -1]  [0 0]
+            [0 1]   [0 0]
+            [1 -1]  [0 0]
+            [1 0]   [0 0]
+            [1 1]   [0 0]}
+           ))
+
+))
