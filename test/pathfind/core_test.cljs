d4a3e13733fb7c1240007190442de0628cc211c8
Crispin Wellington <retrogradeorbit@gmail.com>
2016-11-18 22:58:38 +0800

    add a* step test

--- test/pathfind/core_test.cljs
+++ test/pathfind/core_test.cljs
@@ -2,14 +2,24 @@
   (:require [cljs.test :refer-macros [deftest is]]
             [pathfind.core :as core]))
 
+
+(deftest A*-step
+  (let [start [0 0]
+        result (core/A*-step
+                (core/->state #{} #{} {} {} {})
+                [0 0] [10 10] [0 1])
+        {:keys [closed-set open-set came-from g-score f-score]} result]
+    (println result)
+
+    ))
+
 (deftest reduce-state
   (let [start [0 0]
         result (core/reduce-state-over-neighbors
-                (core/->state #{} #{start} {} {start 0} {start 30})
+                (core/->state #{} #{} {} {} {})
                 [0 0] [10 10] (core/neighbors-for [0 0]))
-        {:keys [closed-set open-set came-from g-score f-score]} result
-        ]
-    (println result)
+        {:keys [closed-set open-set came-from g-score f-score]} result]
+    ; (println result)
     (is (= came-from
            {[-1 -1] [0 0]
             [-1 0]  [0 0]
@@ -18,8 +28,7 @@
             [0 1]   [0 0]
             [1 -1]  [0 0]
             [1 0]   [0 0]
-            [1 1]   [0 0]}
-           ))
+            [1 1]   [0 0]}))
     (is (= open-set
            #{[-1 -1]
              [-1 0]
@@ -28,8 +37,7 @@
              [0 1]
              [1 -1]
              [1 0]
-             [1 1]
-             }))
+             [1 1]}))
     (is (= closed-set
            #{[0 0]}))
     ))
